b3eb072dbd7d0cf62a6f1f9b78584d919ef9d58b
vpk10 <Konstanlera@gmail.com>
2024-12-02 13:47:09 +0700

    добавила второй прикол )

--- test.py
+++ test.py
@@ -7,5 +7,5 @@ class MyTestCase(unittest.TestCase): #glazyrina
         self.assertEqual(True, False)  # add assertion here
 
 
-if __name__ == '__main__':
+if __name__ == '__main__': #konstantinova
     unittest.main()
